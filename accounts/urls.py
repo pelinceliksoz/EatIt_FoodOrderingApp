7f2b3b005265a6682e8bfaf4d52f26a475fc20c7
DESKTOP-CC70KPT\asus <celiksozpelin@gmail.com>
2021-06-14 16:05:21 +0300

    templates will be changed

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/register', views.register, name = 'register'),
+    path('accounts/restaurantRegister', views.restaurantRegister, name='restaurantRegister'),
     path('accounts/login', views.login, name = 'login'),
     path('accounts/restaurantMainPage', views.restaurantMainPage, name = 'restaurantMainPage'),
     path('accounts/logout', views.logout, name='logout')
