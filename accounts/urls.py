63a53181720a0c0950d68a01bc8b6fe463bd4547
DESKTOP-CC70KPT\asus <celiksozpelin@gmail.com>
2021-06-14 14:09:28 +0300

    calc was deleted and new apps were added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register, name = 'register'),
-    path('login', views.login, name = 'login'),
-    path('restaurantMainPage', views.restaurantMainPage, name = 'restaurantMainPage'),
-    path('logout', views.logout, name='logout')
+    path('', views.home, name='home'),
+    path('accounts/register', views.register, name = 'register'),
+    path('accounts/login', views.login, name = 'login'),
+    path('accounts/restaurantMainPage', views.restaurantMainPage, name = 'restaurantMainPage'),
+    path('accounts/logout', views.logout, name='logout')
 ]
--- djangoP/settings.py
+++ djangoP/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'calc'
+    'accounts',
+    'customerApp',
+    'restaurantApp'
 ]
 
 MIDDLEWARE = [
