b3d8a3392622c8dd87c9594a2fef758cb0a25b87
DESKTOP-CC70KPT\asus <celiksozpelin@gmail.com>
2021-06-16 16:28:27 +0300

    registeration done

--- restaurantApp/forms.py
+++ restaurantApp/forms.py
@@ -1,11 +1,15 @@
 from django import forms
-from .models import User,UserModel
+from accounts.models import User, UserModel
 from django.contrib.auth.forms import UserCreationForm
 
-class UserModelForm(UserCreationForm):
+class CreateRestaurantForm(UserCreationForm):
     phone = forms.IntegerField()
     location = forms.CharField()
-
+    category = forms.CharField()
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+
+
+
